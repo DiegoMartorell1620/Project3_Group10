409cda49c62b46bde6a3a87f81eb84bf40a0135c
SmallCad <160338891+SmallCad@users.noreply.github.com>
2024-06-19 21:04:07 -0400

    updated base. includes leaflet and other graphs

--- app.js
+++ app.js
@@ -95,20 +95,20 @@ function Accidentgraph(year) {
       // Extract the neighborhood names and counts for the chart
       let neighborhoods = topNeighborhoods.map(d => d[0]);
       let accidentCounts = topNeighborhoods.map(d => d[1]);
-      console.log(accidentCountByNeighborhood);
+      
 
       // Create a bar chart using Plotly
       let trace = {
         x: neighborhoods,
-        y: accidentCounts,
+        y: accidentCounts ,
         type: 'bar'
       };
       let layout = {
-       // width: 500,  
-        //height: 300,
-        margin: { t: 20, l: 50, r: 50, b: 50 },
+        //width: 500,  
+        height: 500,
         title: `Top 10 Neighborhoods with highest # Accidents in ${year}`,
-        yaxis: { title: '# of Accidents' }
+        yaxis: { title: '# of Accidents', tickfont: {size:10}},
+        xaxis: {tickfont:{size: 10}, tickangle: 90}
       };
       Plotly.newPlot('bar2', [trace], layout);
   });
@@ -141,7 +141,7 @@ function Theftgraph(year) {
       let layout = {
         //width: 500,  // Adjust width as needed
         //height: 300,
-        margin: { t: 20, l: 50, r: 50, b: 50 },
+        margin: { t: 200, l: 50, r: 50,},
         title: `Top 10 Neighborhoods with highest # of Thefts in ${year}`,
         yaxis: { title: '# of thefts' }
       };
@@ -172,7 +172,8 @@ function init() {
       Theftgraph(firstYear);
       Accidentgraph(firstYear);
       googleChart(firstYear);
-      plotlyScatter(firstYear)
+      plotlyScatter(firstYear);
+      leafletmap(firstYear);
 
 
      
@@ -182,8 +183,9 @@ function init() {
       calculateAndDisplaySummary(newYear);
       Theftgraph(newYear);
       Accidentgraph(newYear);
-      googleChart(newyear);
-      plotlyScatter(newyear);
+      googleChart(newYear);
+      plotlyScatter(newYear);
+      leafletmap(newYear);
       
     }
     //Update the event listener to call the optionChanged function when a new year is selected:
@@ -231,7 +233,7 @@ function googleChart(year) {
       let rdCondData = Array.from(rdCond);
       
       // Confirming there are null values
-      console.log(rdCondData);
+      // console.log(rdCondData);
       
       // Rename null values to "unspecified"
       for (data of rdCondData){
@@ -261,8 +263,8 @@ function googleChart(year) {
       'chartType': 'PieChart',
       'containerId': 'chart_div',
       'options': {
-        'width': 600,
-        'height': 600,
+        'width': 500,
+        'height': 500,
         'pieSliceText': 'label',
         'title': "Car Accidents by road conditions"
       },
@@ -294,7 +296,7 @@ function googleChart(year) {
 }};
 
 function plotlyScatter(year) {
-  d3.json('Resources/traffic_accidents_data.json').then((data) => {
+  d3.json('Resources/theft_data.json').then((data) => {
     // Convert year to number
     year = +year;
     // Filter the data for the specified year
@@ -307,24 +309,24 @@ function plotlyScatter(year) {
 
     );
 
-    console.log(theftReportByMonth);
+    console.log(dataForYear);
 
     // Sort the neighborhoods by accident count in descending order and get the top 10
-    let monthlyReport = Array.from(theftReportByMonth )
-      .sort((a, b) => b[1] - a[1]) 
-      .slice(0, 10);
+    let monthlyReport = Array.from(theftReportByMonth );
+      // .sort((a, b) => b[1] - a[1]) 
+      // .slice(0, 10);
 
     // Extract the months and counts for the chart
     let months = monthlyReport.map(d => d[0]);
     let accidentCounts = monthlyReport.map(d => d[1]);
 
-    // console.log(months);
-    // console.log(accidentCounts);
+    console.log(months);
+    console.log(accidentCounts);
 
     // Create a scatter plot using Plotly
     let trace = {
-      y: accidentCounts,
       x: months,
+      y: accidentCounts,
       mode: 'markers',
       type: 'scatter',
       marker: { size: 12 }
@@ -336,6 +338,38 @@ function plotlyScatter(year) {
 
     Plotly.newPlot('scatter', [trace], layout);
 });
+}
+
+let myMap = L.map("map", {
+  center: [43.75107, -79.847015],
+  zoom: 10.5
+});
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+function leafletmap(year) {
+  // Get the data with D3
+  d3.json('Resources/traffic_accidents_data.json').then(function(response) {
+      // Convert year to number
+      year = +year;
+      // Filter the data for the specified year and fatal injuries
+      let dataForYearFatal = response.filter(d => d.YEAR === year && d.ACCLASS === "Fatal");
+      // Clear existing markers from the map
+      myMap.eachLayer(function (layer) {
+          if (layer instanceof L.Marker) {
+              myMap.removeLayer(layer);
+          }
+      });
+      // Add markers to the map based on latitude and longitude for the specified year and fatal injuries
+      dataForYearFatal.forEach(data => {
+          const marker = L.marker([data.LATITUDE, data.LONGITUDE]).addTo(myMap);
+          // You can customize the marker icon, popup, etc. here
+          marker.bindPopup(`<b>${data.STREET1}</b><br>${data.DISTRICT}<br>${data.ACCLASS}`);
+      });
+  });
 }
    
 
@@ -368,139 +402,3 @@ function plotlyScatter(year) {
 
 
 
-
-//------------------------------------------------------------------------
-
-// // Build the metadata panel
-// function buildMetadata(sample) {
-//     d3.json('Resources/traffic_accidents_data.json').then((data) => {
-    
-    
-        
-      
-  
-//       // Filter the metadata for the object with the desired sample number
-//       let resultArray = data.filter(sampleObj => sampleObj.YEAR == sample);
-//       let result = resultArray[0];
-
-//       console.log(result)
-  
-//       // Use d3 to select the panel with id of `#sample-metadata`
-//       let panel = d3.select("#sample-metadata");
-  
-//       // Use `.html("") to clear any existing metadata
-//       panel.html("");
-  
-//       // Inside a loop, you will need to use d3 to append new
-//       // tags for each key-value in the filtered metadata.
-//       Object.entries(result).forEach(([key, value]) => {
-//         let text = `${key.toUpperCase()}: ${value}`;
-//         panel.append("p").text(text);
-//       });
-//     });
-//   }
-  
-//   // function to build both charts
-//   function buildCharts(sample) {
-//     d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-  
-//       // Get the samples field
-//       let samples = data.samples;
-//       // Filter the samples for the object with the desired sample number
-//       let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-//       let result = resultArray[0];
-  
-//       // Get the otu_ids, otu_labels, and sample_values
-//       let otuIds = result.otu_ids;
-//       let otuLabels = result.otu_labels;
-//       let sampleValues = result.sample_values;
-  
-//       // Build a Bubble Chart
-//       let bubbleTrace = {
-//         x: otuIds,
-//         y: sampleValues,
-//         text: otuLabels,
-//         mode: 'markers',
-//         marker: {
-//           size: sampleValues,
-//           color: otuIds,
-//           colorscale: 'Earth'
-//         }
-//       };
-  
-//       let bubbleData = [bubbleTrace];
-  
-//       let bubbleLayout = {
-//         title: 'Bacteria Cultures Per Sample',
-//         xaxis: { title: 'OTU ID' }
-//       };
-  
-  
-//       // Render the Bubble Chart
-  
-//       Plotly.newPlot('bubble', bubbleData, bubbleLayout);  
-  
-//       // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-//       let yTicks = otuIds.slice(0, 10).map(id => `OTU ${id}`).reverse();
-  
-//       // Build a Bar Chart
-//       // Don't forget to slice and reverse the input data appropriately
-//       let barTrace = {
-//         x: sampleValues.slice(0, 10).reverse(),
-//         y: yTicks,
-//         text: otuLabels.slice(0, 10).reverse(),
-//         type: 'bar',
-//         orientation: 'h'
-//       };
-      
-//       let barData = [barTrace];
-      
-//       let barLayout = {
-//         title: 'Top 10 OTUs Found',
-//         xaxis: { title: 'Number of Bacteria' }
-//       };
-  
-//       // Render the Bar Chart
-//       Plotly.newPlot('bar', barData, barLayout);
-//     });
-//   }
-  
-//   // Function to run on page load
-//   function init() {
-//         // Create an array of years from 2018 to 2023
-//         let years = Array.from({length: 6}, (_, index) => 2018 + index);
-      
-//         // Select the dropdown element
-//         let dropdown = d3.select("#selDataset");
-      
-//         // Populate the dropdown with the years
-//         years.forEach(year => {
-//           dropdown.append("option")
-//             .text(year)
-//             .attr("value", year);
-//         });
-      
-//         // Get the first year (2018) from the list
-//         let firstYear = years[0];
-      
-//         // Build charts and metadata panel with the first year
-//         buildMetadata(firstYear);
-//         buildCharts(firstYear);
-//       //Update the optionChanged function to handle the selection of a new year:
-//       function optionChanged(newYear) {
-//         // Build charts and metadata panel each time a new year is selected
-//         buildMetadata(newYear);
-//         buildCharts(newYear);
-//       }
-//       //Update the event listener to call the optionChanged function when a new year is selected:
-//       d3.select("#selDataset").on("change", function() {
-//         // Get the selected year value from the dropdown
-//         let newYear = d3.select(this).property("value");
-//         optionChanged(newYear);
-//       });
-  
-//   }
-  
-//   // Initialize the dashboard
-//   init();
-  
